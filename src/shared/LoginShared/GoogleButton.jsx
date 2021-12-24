4a01bb77e2068c0775cead73d89689b25d8f7b05
George <gnthackray@btinternet.com>
2021-12-24 00:41:33 +0000

    tidy up

--- src/shared/LoginShared/GoogleButton.jsx
+++ src/shared/LoginShared/GoogleButton.jsx
@@ -1,149 +1,66 @@
 import ButtonContent from './button-content'
 import IconGoogle from './icon';
 import React from 'react';
-import blue from '@material-ui/core/colors/blue';
 import { connect } from "react-redux";
-import { withStyles } from '@material-ui/core/styles';
-import {PropTypes} from 'prop-types';
+import {googleButton, defaultStyle} from './styles.jsx';
+import { useTheme } from '@material-ui/core/styles';
+ 
+
+function GoogleButton(props){
+  //console.log(props);
+  const [hovered, setHovered] = React.useState(false);
+  const [active, setActive] = React.useState(false);
+
+  const { tag, type, render, icon, 
+    disabled , mode, onClick, label} = props;
+
+  let state = {
+    hovered: hovered,
+    active: active
+  };
+
+  const theme = useTheme();
+
+  const classes = googleButton(theme);
 
 
-const styles = theme => ({
-  fab: {
-    margin: theme.spacing(1),
-  },
-  extendedIcon: {
-    marginRight: theme.spacing(1),
-  },
-  root: {
-  flexGrow: 1,
-  },
-  grow: {
-    marginLeft: 50,
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginLeft: -12,
-    marginRight: 20,
-  },
-  tolowerBtn : {
-    textTransform: 'none'
-  },
-  avatar: {
-    backgroundColor: blue[100],
-    color: blue[600],
-  },
-  initialStyle : {
-    backgroundColor: theme === 'dark' ? 'rgb(66, 133, 244)' : '#fff',
-    display: 'inline-flex',
-    alignItems: 'center',
-    color: theme === 'dark' ? '#fff' : 'rgba(0, 0, 0, .54)',
-    boxShadow: '0 2px 2px 0 rgba(0, 0, 0, .24), 0 0 1px 0 rgba(0, 0, 0, .24)',
-    padding: 0,
-    borderRadius: 2,
-    border: '1px solid transparent',
-    fontSize: 14,
-    fontWeight: '500',
-    fontFamily: 'Roboto, sans-serif',
-    marginLeft:25
-  },
 
-  hoveredStyle : {
-    cursor: 'pointer',
-    opacity: 0.9
-  },
+  let className = defaultStyle(state, classes, theme );
 
-  activeStyle : {
-    cursor: 'pointer',
-    backgroundColor: theme === 'dark' ? '#3367D6' : '#eee',
-    color: theme === 'dark' ? '#fff' : 'rgba(0, 0, 0, .54)',
-    opacity: 1
-  },
-
-
-});
-
-
-class GoogleButton extends React.Component{
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      hovered: false,
-      active: false
-    };
-
+  let iconGoogleStyle = icon ? classes.google : classes.googleIcon;
+ 
+  let button;
+  if (mode == 'login' || mode == 'logout') {
+    button = <div><IconGoogle key={1} active={active}>  
+                <span key={2} className={iconGoogleStyle}>
+                    {label}
+                  </span>
+                </IconGoogle>  </div>;
+  } else {
+    button = <ButtonContent key ="1">{label}</ButtonContent>;
   }
 
+    return(<button className = {className}
+            onMouseEnter ={()=>{
+              setHovered(true);              
+            }} 
+            onMouseLeave ={()=>{
+              setHovered(false);
+              setActive(false);
+            }} 
+            onMouseDown={()=>{              
+              setActive(true);
+            }} 
+            onMouseUp ={()=>{              
+              setActive(false);
+            }}
+            onClick ={onClick}
+            >
+            {button}
+    </button>);
+   
 
-
-  render() {
-
-    const { tag, type, render, theme, icon,classes, disabled , mode, onClick, label} = this.props;
-
-
-    if (render) {
-      return render({ onClick: onClick(false), disabled });
-    }
-
-    const defaultStyle = () => {
-      if (this.state.active) {
-        if (theme === 'dark')  return classes.initialStyle + ' ' + classes.activeStyle;
-
-        return classes.initialStyle + ' '+classes.activeStyle;
-      }
-
-      if (this.state.hovered)  return classes.initialStyle + ' ' + classes.hoveredStyle;
-
-      return classes.initialStyle;
-    };
-
-    const genericProps = {
-      onMouseEnter: () => this.setState({ hovered: true }),
-      onMouseLeave: () => this.setState({ hovered: false, active: false }),
-      onMouseDown: () => this.setState({ active: true }),
-      onMouseUp: () => this.setState({ active: false }),
-      onClick: ()=> onClick(false),
-      style :{},
-      type,
-      disabled :false,
-      className:defaultStyle()
-    };
-
-    let content;
-
-    if(mode == 'cancel'){
-      content = [
-          <ButtonContent key ="1">
-              {label}
-          </ButtonContent>
-      ];
-    }
-
-    if(mode == 'login' || mode == 'logout'){
-      content = [
-        icon && <IconGoogle key={1} active={this.state.active} />,
-          <ButtonContent icon={icon} key={2}>
-            {label}
-          </ButtonContent>
-      ];
-    }
-
-    return React.createElement(tag, genericProps,content);
-  }
 }
-
-GoogleButton.propTypes = {
-  classes: PropTypes.object.isRequired,
-  label : PropTypes.string,
-  onClick : PropTypes.func,
-  mode: PropTypes.string,
-  disabled : PropTypes.bool,
-  render : PropTypes.func,
-  type: PropTypes.string,
-  tag: PropTypes.string,
-  theme: PropTypes.string,
-  icon: PropTypes.bool,
-};
 
 
 const mapStateToProps = (state, ownProps) => {
@@ -188,4 +105,4 @@ const mapStateToProps = (state, ownProps) => {
 };
 
 
-export default withStyles(styles)(connect(mapStateToProps, ()=>  {return {};})(GoogleButton));
+export default connect(mapStateToProps, ()=>  {return {};})(GoogleButton);
--- src/shared/LoginShared/GooglePopup.jsx
+++ src/shared/LoginShared/GooglePopup.jsx
@@ -3,54 +3,16 @@ import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import React, { Component } from 'react';
-import blue from '@material-ui/core/colors/blue';
-import { withStyles } from '@material-ui/core/styles';
-import {PropTypes} from 'prop-types';
-
-const styles = theme => ({
-  // fab: {
-  //   margin: theme.spacing(1),
-  // },
-  extendedIcon: {
-    marginRight: theme.spacing(1),
-  },
-  root: {
-  flexGrow: 1,
-  },
-  grow: {
-    marginLeft: 50,
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginLeft: -12,
-    marginRight: 20,
-  },
-  tolowerBtn : {
-    textTransform: 'none'
-  },
-  avatar: {
-    backgroundColor: blue[100],
-    color: blue[600],
-  },
-
-});
 
 
-class GooglePopup extends React.Component {
 
-  constructor(props) {
-    super(props)
-  //  //console.log('GooglePopup');
-  }
+function GooglePopup(props){
 
-  handleClose = () => {
-  //  this.props.onClose(false);
-  };
+    var handleClose = () => {
+      //  this.props.onClose(false);
+    };
 
-
-  render() {
-
-    const {googleTokenExpiration,idsTokenExpiration,  open,children } = this.props;
+    const {googleTokenExpiration,idsTokenExpiration,  open,children,ProfileObj } = props;
     
     if(!googleTokenExpiration)
       googleTokenExpiration = Date();
@@ -61,29 +23,28 @@ class GooglePopup extends React.Component {
     // googleTokenExpiration ={googleTokenExpiration}
     // idsTokenExpiration ={idsTokenExpiration}
 
-    if(this.props.ProfileObj == undefined){
-     
+    if(!ProfileObj){
       return null;
     }
 
     return (
-      <Dialog onClose={this.handleClose} aria-labelledby="simple-dialog-title" open = {open}>
+      <Dialog onClose={handleClose} aria-labelledby="simple-dialog-title" open = {open}>
 
         <div>
 
 
           <List>
             <ListItem>
-              <ListItemText primary= "Name" secondary = {this.props.ProfileObj.name} />
+              <ListItemText primary= "Name" secondary = {ProfileObj.name} />
             </ListItem>
             <ListItem>
-              <ListItemText primary= "Email"  secondary={this.props.ProfileObj.email}/>
+              <ListItemText primary= "Email"  secondary={ProfileObj.email}/>
             </ListItem>
             <ListItem>
-              <ListItemText primary= "First Name"  secondary={this.props.ProfileObj.givenName}/>
+              <ListItemText primary= "First Name"  secondary={ProfileObj.givenName}/>
             </ListItem>
             <ListItem>
-              <ListItemText primary= "Surname" secondary={this.props.ProfileObj.familyName}/>
+              <ListItemText primary= "Surname" secondary={ProfileObj.familyName}/>
             </ListItem>
             <ListItem>
               <ListItemText primary= "Google Expiration"  secondary={googleTokenExpiration.toLocaleString("en-GB")}/>
@@ -102,13 +63,9 @@ class GooglePopup extends React.Component {
         </div>
       </Dialog>
     );
-  }
+  
 }
 
-GooglePopup.propTypes = {
-  classes: PropTypes.object.isRequired,
-  onClose: PropTypes.func,
-  selectedValue: PropTypes.string,
-};
 
-export default withStyles(styles)(GooglePopup);
+
+export default GooglePopup;
