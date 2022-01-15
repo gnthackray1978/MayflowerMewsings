8478fa71ef774fdb2b007c2b13243ee01f50b5ed
George <gnthackray@btinternet.com>
2022-01-15 22:00:08 +0000

    add in extra domain info

--- src/shared/IDSConnect/idsReducers.js
+++ src/shared/IDSConnect/idsReducers.js
@@ -12,9 +12,24 @@ export default (state = {
     IsExternalLoginOnly :true,
     silent_redirect_uri: 'http://localhost:1234',
     automaticSilentRenew: true,
-    google_token_uri :'https://msgauth01.azurewebsites.net/token'
-
+    google_token_uri :'https://msgauth01.azurewebsites.net/token',
+    AllowOfflineAccess : true
   },
+  GNTServParams :{
+    authority: "https://msgauth01.azurewebsites.net",
+    client_id: "gnthackray",
+    redirect_uri: "http://gnthackray.co.uk",
+    response_type: "code",
+    scope:"openid profile api1",
+    post_logout_redirect_uri: "http://gnthackray.co.uk",
+    loadUserInfo: true,
+    IsExternalLoginOnly :true,
+    silent_redirect_uri: "http://gnthackray.co.uk",
+    automaticSilentRenew: true,
+    google_token_uri :'https://msgauth01.azurewebsites.net/token',
+    AllowOfflineAccess : true
+  },
+
 
   infoloaded :false,
   IdsLogInDetailsVisible :false
